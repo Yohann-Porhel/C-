ca65125e9535ac424fe320dd4d35627e74eee2ef
Yohann-Porhel <yohann.porhel@laposte.net>
2018-09-16 10:58:41 +0200

    Add 2 codes

---
+++ IPAdress/IPAdress/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPAdress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your IP Adress :");
+            string adress = Console.ReadLine();
+            string[] parts = adress.Split('.');
+            string message = "";
+
+            if (parts.Length == 4)
+            {
+                foreach (string part in parts)
+                {
+                    if ((Int32.Parse(part)) >= 0 && (Int32.Parse(part)) <= 255)
+                    {
+                        message = "{0} is a valid IP Adress";
+                    }
+                    else
+                    {
+                        message = "{0} is not a valid IP Adress";
+                        break;
+                    }
+                }                
+            }
+            else
+            {
+                message = "{0} is not a valid IP Adress";
+            }
+            Console.WriteLine(message, adress);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PalindromeNumbers/PalindromeNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumbers
+{
+    public static class Palindrome
+    {
+
+        // METHOD
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        // MAIN
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number :");
+            string input = Console.ReadLine();
+            string reverse = Reverse(input);
+            char[] charsInput = input.ToCharArray();
+            char[] charsReverse = reverse.ToCharArray();
+
+            for (int positionChar = 0; positionChar < charsReverse.Length;)
+            {
+                string message = "";
+                for (int posCharReverse = 0; posCharReverse < charsReverse.Length; posCharReverse++)
+                {
+                    if ((charsReverse[posCharReverse]) != (charsInput[positionChar]))
+                    {
+                        message = "{0} isn't a palindrome";
+                        break;
+                    }
+                    else
+                    {
+                        positionChar++;
+                        message = "{0} is a palindrome";
+                    }
+                }
+                Console.WriteLine(message, input);
+                break;
+            }
+            Console.Read();
+        }
+    }
+}
