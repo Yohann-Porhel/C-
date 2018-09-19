d75a867c36b06b9930f5a32ce441b9a5f54dde6d
Yohann-Porhel <yohann.porhel@laposte.net>
2018-09-19 10:50:22 +0200

    Trimorphic numbers

---
+++ Trimorphic/Trimorphic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trimorphic
+{
+    // A trimorphic number is a number whose cube ends in the number itself
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First number of the raw ?");
+            string first = Console.ReadLine();
+            Console.WriteLine("Last number of the raw ?");
+            string last = Console.ReadLine();
+
+            double increment = Double.Parse(first);
+            double maxrange = Double.Parse(last);
+
+            do
+            {
+                double cube = Math.Pow(increment, 3);
+                string result = cube.ToString();
+                if (result.EndsWith(increment.ToString()))
+                {
+                    Console.WriteLine("{0} is a trimorphic number", increment.ToString());
+                }
+                increment++;
+
+            } while (increment <= maxrange);
+            Console.ReadKey();
+        }
+
+    }
+
+}
